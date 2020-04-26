5600f7aed69ce0f47237dd611ccc9aba9a0c3288
Vincent Fleischacker <vdynmx@gmail.com>
2020-04-26 13:59:23 +0200

    creating movie object to parse into local js array

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -6,6 +6,8 @@ const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelector('input');
 
+const movies = [];
+
 const toggleBackdrop = () => {
     backDropOn.classList.toggle('visible');
 };
@@ -19,7 +21,7 @@ const cancelMovieModal = () => {
     toggleMovieModal();
 };
 
-const addMovieHandler = () => {
+const addMovieHandler = () => { // function structure first pulling in data from external fields from thml, then validating with if statement, then creating new object, parsing that data into object that pushing that new object into the parent array
     const titleValue = userInputs[0].value;
     const imageUrlValue = userInputs[1].value;
     const ratingValue = userInputs[2].value;
@@ -34,6 +36,15 @@ const addMovieHandler = () => {
         alert('Please enter valid rating between 1 and 5');
         return;
     }
+
+    const newMovie = { // creating a new object with properties
+        title: titleValue,
+        image: imageUrlValue,
+        rating: ratingValue
+    };
+    movies.push(newMovie); //accessing a parental array to push a value into it from the objects created in this function
+    console.log(movies);
+    toggleMovieModal();
 };
 
 const backdropClickHandler = () => {
