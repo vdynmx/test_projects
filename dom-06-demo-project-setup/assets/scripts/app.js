547c1d4db43ba8d4287651ba788bff72481b4f74
Vincent Fleischacker <vdynmx@gmail.com>
2020-04-26 11:25:45 +0200

    user input verification

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,16 +1,39 @@
 const addMovieModal = document.getElementById('add-modal');
 console.log(addMovieModal);
+const backDropOn = document.getElementById('backdrop');
 const startAddMovieButton = document.querySelector('header button');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
-const backDropOn = document.getElementById('backdrop');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelector('input');
 
 const toggleBackdrop = () => {
     backDropOn.classList.toggle('visible');
-}
+};
 
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
     toggleBackdrop();
+};
+
+const cancelMovieModal = () => {
+    toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+    const titleValue = userInputs[0].value;
+    const imageUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
+
+    if (
+        titleValue.trim() == '' ||
+        imageUrlValue.trim() == '' ||
+        ratingValue.trim() == '' ||
+        +ratingValue < 1 ||
+        +ratingValue > 5
+    ) {
+        alert('Please enter valid rating between 1 and 5');
+        return;
+    }
 };
 
 const backdropClickHandler = () => {
@@ -19,5 +42,6 @@ const backdropClickHandler = () => {
 
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
-cancelAddMovieButton.addEventListener('click', toggleMovieModal);
+cancelAddMovieButton.addEventListener('click', cancelMovieModal);
 backDropOn.addEventListener('click',backdropClickHandler);
+confirmAddMovieButton.addEventListener('click', addMovieHandler);
