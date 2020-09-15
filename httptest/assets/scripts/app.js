b48e0bf2a58be6ad5d4dfb68768eba704c97259a
Vincent Fleischacker <vdynmx@gmail.com>
2020-09-15 14:11:43 +0200

    response type json

--- httptest/assets/scripts/app.js
+++ httptest/assets/scripts/app.js
@@ -4,8 +4,11 @@ const xhr = new XMLHttpRequest(); //?? Why do I need to creat an object to sent 
 //to start configure request
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts'); //first step twords confirguing request. First argument to to set the type of action. Second argument is to URL
 
+xhr.responseType = 'json';
+
 xhr.onload = function() {
-    const listOfPosts = JSON.parse(xhr.response); //Storing the converted JS values from the XHR JSON response in listofPosts
+    //const listOfPosts = JSON.parse(xhr.response); //Storing the converted JS values from the XHR JSON response in listofPosts
+    const listOfPosts = xhr.response; //because we set response type to json above we dont have to parse it in
     console.log(listOfPosts);
 };
 
