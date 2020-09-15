9d8bfb593db8a9dcafd4f1ccb30ebc6d7ac152ce
Vincent Fleischacker <vdynmx@gmail.com>
2020-09-15 15:44:06 +0200

    Promisifying HTTP requests

--- httptest/assets/scripts/app.js
+++ httptest/assets/scripts/app.js
@@ -1,4 +1,4 @@
-
+/*
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 
@@ -25,5 +25,47 @@ xhr.onload = function() {
 xhr.send();
 
 //JSON method stringify converts JS value to JSON string. parse converts JSON string to JS value. Format / structure wise so it can be used
+*/
+// ------------------------------------------------------------------------
+// Restructuring code for Post request with promises
 
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
 
+//gonna create a function that can both get and post methods
+function sendHttpRequest(method, url) {
+    const promise = new Promise((resolve, reject) => {
+       const xhr = new XMLHttpRequest();
+
+    //to start configure request
+    xhr.open(method, url); 
+    
+    xhr.responseType = 'json';
+    
+    xhr.onload = function() {
+        resolve(xhr.response);
+    };
+    
+    xhr.send();  
+    });
+    return promise;
+
+}
+
+async function fetchPosts() {
+    const responseData = await sendHttpRequest(
+        'GET', 
+        'https://jsonplaceholder.typicode.com/posts'
+        );
+    
+      const listOfPosts = responseData; 
+        for (const post of listOfPosts) {
+            const postEl = document.importNode(postTemplate.content, true);
+            postEl.querySelector('h2').textContent = post.title.toUpperCase();
+            postEl.querySelector('p').textContent = post.body;
+            listElement.append(postEl);
+        }  
+}
+    
+
+fetchPosts();
