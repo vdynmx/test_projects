dca09bc4cbe8536e207c2f8263c2edf5f4b72010
Vincent Fleischacker <vdynmx@gmail.com>
2020-10-12 15:00:20 +0200

    Factory Function notes

--- Topics/factory-functions/app.js
+++ Topics/factory-functions/app.js
@@ -1,3 +1,4 @@
+// Pure Function
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -8,7 +9,7 @@ console.log(add(1, 5)); // 6
 console.log(add(12, 15)); // 27
 
 function addRandom(num1) {
-  return num1 + Math.random();
+  return num1 + Math.random(); // random create a side effect of chaning value outside the function
 }
 
 console.log(addRandom(5));
@@ -17,20 +18,23 @@ let previousResult = 0;
 
 function addMoreNumbers(num1, num2) {
   const sum = num1 + num2;
-  previousResult = sum;
+  previousResult = sum; // Side Effect of change a variable outside the function
   return sum;
 }
 
 console.log(addMoreNumbers(1, 5));
 
-const hobbies = ['Sports', 'Cooking'];
+const hobbies = ['Sports', 'Cooking']; 
 
 function printHobbies(h) {
-  h.push('NEW HOBBY');
-  console.log(h);
+  h.push('NEW HOBBY'); // side effect of changing the array outside the function
+  console.log(h); // arrays are objects and objects are reference values
 }
 
 printHobbies(hobbies);
+
+
+//-------------- Factory Function
 
 function createTaxCalculator(tax) {
   function calculateTax(amount) {
